f5131d22b56311c3d88135086fed75bc02d41696
harui <61614490+harui2019@users.noreply.github.com>
2023-11-27 21:45:09 +0800

    build: - tailwind/daisy ui setup completed - add framer-motion - add classnames

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,9 +22,10 @@ const config: Config = {
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    prefix: "daisy-", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ":root", // The element that receives theme color CSS variables
+    rtl: false, // RTL direction support
     themes: ["dark", "light"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     // themes: [
     //   {
