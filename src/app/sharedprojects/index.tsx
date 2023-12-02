19d1f970b08578f4231d84ee948e383f180c1cab
harui <61614490+harui2019@users.noreply.github.com>
2023-12-03 04:25:27 +0800

    feat(page): add router sharedprojects

---
+++ src/app/sharedprojects/index.tsx
@@ -0,0 +1,11 @@
+// hooks
+import { useUnitedResize } from "../../hooks";
+// components
+// styles
+import classnames from "classnames";
+
+const Home = () => {
+  return <div className={classnames("h-full w-full")}></div>;
+};
+
+export default Home;
