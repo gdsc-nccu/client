32edf63b6de4afb5096c56015cac408624a0ebfd
harui <61614490+harui2019@users.noreply.github.com>
2023-12-03 00:33:17 +0800

    fix(logo): attr rename

--- src/assets/icon/user.tsx
+++ src/assets/icon/user.tsx
@@ -11,9 +11,9 @@ export default function UserIcon(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={classnames("feather feather-user", className)}
       {...otherProps}
     >
