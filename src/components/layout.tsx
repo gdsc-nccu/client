05b285aca17f6f742a49a4ae750f185d55c128b8
harui <61614490+harui2019@users.noreply.github.com>
2023-12-02 17:24:54 +0800

    feat(profile): profile reversed color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,35 @@
 // utils
 import type { ReactElement } from "react";
 // hooks
-import { useUnitedResize } from "../hooks";
+import { useUnitedResize, useCurrentPath } from "../hooks";
 import { useRef } from "react";
 // components
 import Header from "./navigation/header";
 import Footer from "./navigation/footer";
+// styles
+import classnames from "classnames";
 
-export default function Layout({ children }: { children: ReactElement }) {
+export default function Layout({
+  children,
+  className,
+}: {
+  children: ReactElement;
+  className: string;
+}) {
   const ref = useRef<HTMLDivElement>(null);
   // const isLogin = useAppSelector((state) => state.User.isLogin);
   const { width, height, isBreak } = useUnitedResize("mobileBreak", ref);
+  const pathname = useCurrentPath();
 
   return (
     <div
-      className="flex w-full min-h-screen flex-col items-center justify-between"
+      className={classnames(
+        "flex w-full min-h-screen flex-col items-center justify-between",
+        "transition-colors duration-300 ease-in-out",
+        { "bg-base-content text-base-100": pathname !== "profile" },
+        { "bg-base-100 text-base-content": pathname === "profile" },
+        className,
+      )}
       ref={ref}
     >
       <Header
