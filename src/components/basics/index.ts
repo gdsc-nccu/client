3f12f215512ab7180556f8fc9a5e7e2c71668234
harui <61614490+harui2019@users.noreply.github.com>
2023-12-02 17:24:17 +0800

    feat(basic): add toggle

--- src/components/basics/index.ts
+++ src/components/basics/index.ts
@@ -2,5 +2,6 @@ import Modal from "./modal/Modal";
 import Backdrop from "./backdrop/Backdrop";
 import IntroBackdrop from "./backdrop/IntroBackdrop";
 import Burger from "./burger/Burger";
+import Toggle from "./toggle/Toggle";
 
-export { Modal, Backdrop, IntroBackdrop, Burger };
+export { Modal, Backdrop, IntroBackdrop, Burger, Toggle };
