0c2c4d52660ddc79e9fca6d8eef8b910a6e7ca35
harui <61614490+harui2019@users.noreply.github.com>
2023-12-11 00:06:22 +0800

    feat(Login): Login page update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,30 @@
 "use client";
 // utils
-import React from "react";
+import React, { useRef } from "react";
 import { signIn } from "next-auth/react";
-import Logo from "/public/logo-white.png";
+// hooks
+import { useUnitedResize } from "../hooks";
 // components
+import Logo from "/public/logo-white.png";
 import Image from "next/image";
+// style
+import classnames from "classnames";
 
 const Login = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { isBreak } = useUnitedResize("mobileBreak", ref);
+
   return (
-    <div className="w-full h-screen flex flex-col justify-center place-items-center bg-base-300">
+    <div
+      className={classnames(
+        "w-full h-screen flex flex-col justify-center place-items-center",
+        "transition-colors duration-200 ease-in-out",
+        isBreak
+          ? "bg-base-300 text-base-content"
+          : "bg-base-100 text-base-content",
+      )}
+      ref={ref}
+    >
       <Image src={Logo} alt="" />
       <button
         onClick={() => signIn("google")}
