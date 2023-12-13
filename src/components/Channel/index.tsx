067c8ad07ccd3e8419dd65f355a9d90372d42423
harui <61614490+harui2019@users.noreply.github.com>
2023-12-13 22:32:39 +0800

    feat(Channel): props update

--- src/components/Channel/index.tsx
+++ src/components/Channel/index.tsx
@@ -2,52 +2,91 @@
 import React from "react";
 // components
 import GDSCBracketWhite from "../../assets/icon/gdsc-bracket-white";
+import ChannelTitle from "./ChannelTitle";
+import Link from "next/link";
+import Image from "next/image";
 // styles
 import classnames from "classnames";
 import "./index.css";
 // utils
-import Link from "next/link";
 
 interface ChannelProps {
   channels: Channel[];
+  showTitle?: boolean;
   classNameContainer?: string;
   classNameChannel?: string;
 }
 
 const Channel = ({
   channels,
+  showTitle,
   classNameContainer,
   classNameChannel,
 }: ChannelProps) => {
   return (
     <div
       className={classnames(
-        "w-full flex flex-row overflow-auto space-x-[2vw] px-16 p-8",
+        "w-full flex flex-row overflow-auto space-x-[2rem] px-16 p-8",
         classNameContainer,
       )}
     >
       {channels.map((channel, index) => (
-        <div
-          className={classnames("rounded-3xl bg-info", classNameChannel)}
-          key={index}
-        >
-          {channel.link === undefined ? (
-            <div
-              className={classnames(
-                "h-full w-full relative",
-                "flex flex-col spaxe-y-1 justify-center items-center opacity-60",
-              )}
-            >
-              {/* <p className="absolute top-2 left-3">{index}</p> */}
-              <GDSCBracketWhite className="text-4xl" />
-              <p className="text-2xl use-product-sans font-bold tracking-wider">
-                No Sinal
-              </p>
-            </div>
-          ) : (
-            <Link className="h-full w-full" href={channel.link}>
+        <div key={index} className="flex flex-col">
+          <div
+            className={classnames(
+              "rounded-3xl bg-info border-2 border-base-content",
+              classNameChannel,
+            )}
+          >
+            {channel.link === undefined ? (
+              <div
+                className={classnames(
+                  "h-full w-full relative",
+                  "flex flex-col spaxe-y-1 justify-center items-center opacity-60",
+                )}
+              >
+                {/* <p className="absolute top-2 left-3">{index}</p> */}
+                <GDSCBracketWhite className="text-4xl" />
+                <p className="text-2xl use-product-sans font-bold tracking-wider">
+                  No Sinal
+                </p>
+              </div>
+            ) : (
+              <>
+                <Link className="h-full w-full relative" href={channel.link}>
+                  {channel.preview ? (
+                    <Image
+                      src={channel.preview}
+                      alt={
+                        (channel.title ? channel.title : "No Title") +
+                        " preview"
+                      }
+                      width={2000}
+                      height={2000}
+                      className="rounded-3xl w-full h-full"
+                    />
+                  ) : (
+                    <div
+                      className={classnames(
+                        "h-full w-full relative",
+                        "flex flex-col spaxe-y-1 justify-center items-center opacity-60",
+                      )}
+                    >
+                      {/* <p className="absolute top-2 left-3">{index}</p> */}
+                      <GDSCBracketWhite className="text-4xl" />
+                      <p className="text-2xl use-product-sans font-bold tracking-wider">
+                        No Sinal
+                      </p>
+                    </div>
+                  )}
+                </Link>
+              </>
+            )}
+          </div>
+          {showTitle && (
+            <p className="text-2xl use-product-sans font-bold tracking-wider flex justify-center m-2">
               {channel.title}
-            </Link>
+            </p>
           )}
         </div>
       ))}
@@ -56,3 +95,4 @@ const Channel = ({
 };
 
 export default Channel;
+export { ChannelTitle };
