f3825dc60a0ff0bb24fee485c7e28dfa8c498232
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-17 19:12:10 +0800

    feat(next-auth): add firestore adapter to authOption

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { adminAuth, adminFirestore } from "../../../../utils/firebase";
+
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -9,7 +12,25 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET as string,
     }),
   ],
+  adapter: FirestoreAdapter(adminFirestore),
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn(params) {
+      if (params.user) {
+        try {
+          await adminAuth.getUserByEmail(params.user.email as string);
+        } catch (error) {
+          await adminAuth.createUser({
+            uid: params.account?.userId,
+            email: params.user.email as string,
+            emailVerified: true,
+          });
+        }
+        return true;
+      }
+      return false;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
